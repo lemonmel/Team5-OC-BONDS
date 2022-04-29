8ba24287e8ad5559d731ef0f82bff15466487db6
lemonmel <76807277+lemonmel@users.noreply.github.com>
2022-04-29 11:08:57 +0800

    Update profile.js
    fix bugs

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -127,17 +127,19 @@ function deleteAccount() {
 
 /* Toggle Sidebar */
 var initialTogglestate = true
-if (initialTogglestate) {
+if(initialTogglestate){
   document.getElementById("toggleSideButton").innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-chevron-compact-down' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z'/></svg>"
 }
 
-function toggleSide() {
+
+function toggleSide(){
   console.log("lol")
-  if (initialTogglestate) {
+  if(initialTogglestate){
     document.getElementById("profileSideBar").style.display = "none"
     document.getElementById("minimizeProfileSide").style.display = "flex"
   }
-  else {
+  else{
+    document.getElementById("profileSideBar").style.display = "block"
     document.getElementById("minimizeProfileSide").style.display = "none"
   }
   initialTogglestate = !initialTogglestate
